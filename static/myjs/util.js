5a09ef78db563cbda9891b06acb354e028da2171
libo <libo01@51talk.com>
2022-03-24 10:36:45 +0800

    'icon'

--- static/myjs/util.js
+++ static/myjs/util.js
@@ -17,3 +17,44 @@ function showMessage(message, type = "danger", localhost = "") {
         }
     },2000)
 }
+
+/**
+ * 图标
+ * @param fileType
+ */
+function getIcon(fileType = '', category = 0) {
+    var img = '/static/icon/';
+    switch (category) {
+        case 0:
+            img += 'folder.png';
+            break;
+        case 1:
+            img += 'image.png';
+            break;
+        case 2:
+            if (fileType == 'doc' || fileType == 'docx') {
+                img += 'word.png';
+            } else if(fileType == 'ppt' || fileType == 'pptx') {
+                img += 'ppt.png';
+            } else {
+                img += 'txt.png';
+            }
+            break;
+        case 3:
+            img += 'video.png';
+            break;
+        case 4:
+            img += 'mp3.png';
+            break;
+        case 5:
+            img += 'attachment.png';
+            break;
+        case 6:
+            img += 'unknown.png';
+            break;
+        default:
+            break;
+    }
+
+    return img;
+}
